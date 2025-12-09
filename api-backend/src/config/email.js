1980aed458e44c96dbbfe069fa9da9d27d2d4335
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2025-12-09 16:36:45 +0530

    - done

--- api-backend/src/config/email.js
+++ api-backend/src/config/email.js
@@ -1,14 +1,23 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.CLINIC_EMAIL,
+//     pass: process.env.CLINIC_EMAIL_PASS,
+//   },
+// });
+
+// module.exports = transporter;
+
+
+// config/email.js
+const { Resend } = require("resend");
 require("dotenv").config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.CLINIC_EMAIL,
-    pass: process.env.CLINIC_EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-module.exports = transporter;
+module.exports = resend;
