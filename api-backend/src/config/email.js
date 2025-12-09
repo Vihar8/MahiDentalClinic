a118bdc2aba783cbe6360173499437ed3dafd42b
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2025-12-09 09:53:10 +0530

    -jj

--- api-backend/src/config/email.js
+++ api-backend/src/config/email.js
@@ -2,8 +2,9 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "smtp-relay.brevo.com",
+  host: "smtp.gmail.com",
   port: 587,
+  secure: false,
   auth: {
     user: process.env.CLINIC_EMAIL,
     pass: process.env.CLINIC_EMAIL_PASS,
