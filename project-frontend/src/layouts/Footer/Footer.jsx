5a61c12994e79bd3a4ccc36a7f5e8dfb5fbc6d6c
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2025-12-25 09:43:07 +0530

    - Done

--- project-frontend/src/layouts/Footer/Footer.jsx
+++ project-frontend/src/layouts/Footer/Footer.jsx
@@ -124,7 +124,7 @@ const Footer = () => {
         <Container>
           <p className={`${classes.copyrightText}`}>
             Copyright &copy; {getCurrentYear()} Mahi Dental Clinic.
-            All rights reserved | Designed by <a href="" target="_blank" rel="noopener noreferrer">Sujal & Vihar</a>
+            All rights reserved | Design & Developed by <a href="" target="_blank" rel="noopener noreferrer" className="font-bold"> Sujal & Vihar</a>
           </p>
         </Container>
       </div>
